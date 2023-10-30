0dbe55580b359455955341184d0d9553149f2783
danielbatres <danielbatresuser@gmail.com>
2023-10-30 13:08:05 -0600

    HTTP requests with Requests package

---
+++ web-server/store.py
@@ -0,0 +1,13 @@
+import requests
+
+def get_categories():
+  request = requests.get('https://api.escuelajs.co/api/v1/categories')
+
+  print(request.status_code)
+  print(request.text)
+  print(type(request.text))
+
+  categories = request.json()
+
+  for category in categories:
+    print(category['name'])
